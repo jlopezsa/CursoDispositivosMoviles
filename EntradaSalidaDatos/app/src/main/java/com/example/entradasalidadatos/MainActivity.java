bcf6d9510dfe5712d095dfdb3f867ae92aba78f3
Julian Lopez <jlopezsa@gmail.com>
2021-08-29 09:39:48 -0500

    Entrada y salida de datos. Se ingresa un número, se oprime un boton y aparece un mensaje con el número ingresado

---
+++ EntradaSalidaDatos/app/src/main/java/com/example/entradasalidadatos/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.entradasalidadatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView salida;
+    private EditText operador1;
+    private Button boton;
+    private int numeroOperador;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        salida = (TextView) findViewById(R.id.txtSalida);
+        operador1 = (EditText) findViewById(R.id.txtOperador1);
+        boton = (Button) findViewById(R.id.btnEnvio);
+
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numeroOperador = Integer.parseInt(operador1.getText().toString());
+
+                salida.setText("El número ingresado es: " + numeroOperador);
+
+                Log.i("FLAG(MainActivity)", "El valor ingresado es: "+ numeroOperador); // Mensaje en Logcat
+            }
+        });
+
+        //salida.setText("Prueba desde codigo");
+
+    }
+}
